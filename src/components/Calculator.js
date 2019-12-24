261bc1c3b9b3bfd43af4cf81fcc9ab5be7fb2373
Larisa Popescu <larisa@m33.be>
2019-12-24 10:04:43 +0100

    added comments and limited the digits after decimal

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -45,20 +45,25 @@ export const Calculator = () => {
 		let tempFormula = formula;
 		let tempDisplay = display;
 
-		//If the user last pressed =, then start a new formula
+		//If the user last pressed = and then presses a number, then start a new formula
 		let index = tempFormula.indexOf('=');
 		if (index > 0) {
 			tempFormula = '';
 			tempDisplay = '0';
 		}
 
+		//don't let the formula start with 0. 005 becomes 5. If it is after a decimal, then 00 is fine
 		if (tempFormula.endsWith('0') && !tempDisplay.includes('.')) {
+			//if the display shows 0,... which is < 1, then the formula can end in 0 (I use display because the formula can have already more sets of numbers with a decimal)
 			tempFormula = tempDisplay - 0 > 1 ? tempFormula : tempFormula.slice(tempFormula.lenght, -1);
 		}
 
+		//the initial state of display starts from empty string when you start typing a number
 		if (tempDisplay === '0') {
 			tempDisplay = '';
 		}
+
+		//if the user last pressed an operator, delete it before you write the next number
 		if (endsWithOperator(tempDisplay)) {
 			tempDisplay = tempDisplay.slice(1);
 		}
@@ -66,6 +71,7 @@ export const Calculator = () => {
 		tempDisplay = tempDisplay + event.target.value;
 		tempFormula = tempFormula + event.target.value;
 
+		//place a max digit limit
 		if (tempDisplay.length > 21) {
 			tempDisplay = tempDisplay.slice(tempDisplay.lenght, -1);
 			tempFormula = tempFormula.slice(tempFormula.lenght, -1);
@@ -87,6 +93,7 @@ export const Calculator = () => {
 			tempDisplay = '0';
 		}
 
+		//handle decimals
 		if (endsWithOperator(tempDisplay) || tempDisplay === '0') {
 			tempDisplay = '0.';
 			tempFormula = tempFormula.endsWith('0') ? tempFormula + '.' : tempFormula + '0.';
@@ -95,6 +102,7 @@ export const Calculator = () => {
 			tempDisplay = tempDisplay.includes('.') ? tempDisplay : tempDisplay + '.';
 		}
 
+		//place a max digit limit
 		if (tempDisplay.length > 21) {
 			tempDisplay = tempDisplay.slice(tempDisplay.lenght, -1);
 			tempFormula = tempFormula.slice(tempFormula.lenght, -1);
@@ -123,7 +131,10 @@ export const Calculator = () => {
 	};
 
 	const evaluate = () => {
-		setFormula(formula + '=' + eval(formula));
+		//only show the first 12 decimals
+		let result = Math.round(1000000000000 * eval(formula)) / 1000000000000;
+
+		setFormula(formula + '=' + result);
 		setDisplay(eval(formula));
 	};
 
